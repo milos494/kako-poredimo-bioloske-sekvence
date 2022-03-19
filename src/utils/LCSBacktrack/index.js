6cdd61d31aeb895e3a93a70db72740a947dcec37
milos494 <milosloncarevic494@gmail.com>
2022-03-19 17:24:15 +0100

    lint fix

--- src/utils/LCSBacktrack/index.js
+++ src/utils/LCSBacktrack/index.js
@@ -4,15 +4,14 @@ const LCSBacktrack = (sequence1, sequence2) => {
   const backtrack = { "0;0": "-1;-1" };
   const S = { "0;0": 0 };
   for (let i = 1; i < width + 1; i += 1) {
-    // S[`0;${i}`] = S[`0;${i - 1}`] + manhattanMap[`0;${i - 1}`][`0;${i}`];
     backtrack[`0;${i}`] = `0;${i - 1}`;
     S[`0;${i}`] = 0;
   }
 
   for (let j = 1; j < height + 1; j += 1) {
-    // S[`${j};0`] = S[`${j - 1};0`] + manhattanMap[`${j - 1};0`][`${j};0`];
     backtrack[`${j};0`] = `${j - 1};0`;
     S[`${j};0`] = 0;
+  }
 
   for (let i = 1; i < height + 1; i += 1) {
     for (let j = 1; j < width + 1; j += 1) {
@@ -20,9 +19,6 @@ const LCSBacktrack = (sequence1, sequence2) => {
       const fromLeft = S[`${i};${j - 1}`] + 0;
       const fromDiagonal =
         S[`${i - 1};${j - 1}`] + +(sequence1[j - 1] === sequence2[i - 1]);
-      // if (sequence1[i - 1] === sequence2[j - 1]) {
-      //   debugger;
-      // }
 
       S[`${i};${j}`] = Math.max(fromTop, fromLeft, fromDiagonal);
 
@@ -31,7 +27,6 @@ const LCSBacktrack = (sequence1, sequence2) => {
       } else if (S[`${i};${j}`] === fromLeft) {
         backtrack[`${i};${j}`] = `${i};${j - 1}`;
       } else {
-        // debugger;
         backtrack[`${i};${j}`] = `${i - 1};${j - 1}`;
       }
     }
@@ -44,6 +39,7 @@ const LCSBacktrack = (sequence1, sequence2) => {
     return previous;
   }, {});
 
+  console.log(track);
 
   return { track, S };
 };
